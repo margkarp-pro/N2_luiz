037c26884c5330840d179e1dba2d9be3b01d5ada
margkarp-pro <lucas07.fernandes@catolicasc.edu.br>
2026-05-31 20:21:15 -0300

    get user upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const dados = []
+let user_tag = 0;
+
 
 function get_user(){
-
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(function(dado_retornado_user){
+    return dado_retornado_user.json()
+}).then(function(json){
+    dados.push(...json);
+    console.log(dados)
+})
 }
 
 function get_post(){
