ff675a5d05d2846ca587aa5cd1a07d0e3ec2c4c2
raquel1s <silvaraquel.0729@gmail.com>
2026-05-31 20:21:56 -0300

    feat: get post e create post cards

--- script.js
+++ script.js
@@ -5,7 +5,28 @@ function get_user(){
 }
 
 function get_post(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json){
+        dados.push(...json);
+        create_post_cards();
+    })
+}
 
+function create_post_cards(){
+    const cardContent = document.querySelector('.card-content');
+    cardContent.innerHTML = '';
+    dados.forEach(post => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        `;
+        cardContent.appendChild(card);
+    });
 }
 
 function reset_window(){
