c1a462a48a5247886d0f2e2185c1b8c7c4e3af4a
margkarp-pro <lucas07.fernandes@catolicasc.edu.br>
2026-05-31 21:37:08 -0300

    forms added

--- script.js
+++ script.js
@@ -43,9 +43,11 @@ function reset_window(type) {
     if (type === 'posts') {
         dados.length = 0;
         get_post();
+        create_post_forms();
     } else if (type === 'users') {
         dados.length = 0;
         get_user();
+        create_user_forms();
     }
 }
 
@@ -65,8 +67,34 @@ function create_user_cards() {
     });
 }
 
-function add(){
+function add_post(){
+    const dado_titulo = document.getElementById("title");
+    const dado_body = document.getElementById("body");
+    dados.push({
+        id: dados.length+1,
+        title: dado_titulo.value,
+        body: dado_body.value
+    })
+    dado_titulo.value='';
+    dado_body.value='';
+    create_post_cards();
+}
 
+function add_user(){
+    const dado_username = document.getElementById("username");
+    const dado_name = document.getElementById("name");
+    const dado_email = document.getElementById("email");
+    dados.push({
+        id: dados.length+1,
+        username: dado_username.value,
+        name: dado_name.value,
+        email: dado_email.value
+    });
+    dado_username.value='';
+    dado_name.value='';
+    dado_email.value='';
+    create_user_cards();
+    
 }
 
 function deletes(id){
@@ -95,7 +123,7 @@ function create_user_forms(){
             <label for="email">Email: </label><br>
             <input type="text" id="email" name="email"><br><br>
         </form>
-        <button>adicionar user</button>
+        <button onclick="add_user()">adicionar user</button>
     `
 }
 
